2594f68b3143b9b561fb321b7b305590e0e2925d
numataken <numataken001@gmail.com>
2023-10-19 14:31:39 +0900

    ソースコードを追加

--- osx_drawpad-master/DrawPad/main.cpp
+++ osx_drawpad-master/DrawPad/main.cpp
@@ -34,5 +34,8 @@ void DrawMain()
     DrawText("Hello, world!!", -12*14/2, -44, kColorRed);
     Sleep(0.5f);
     DrawText("Hello, world!!", -12*14/2, -66, kColorGray);
+    
+    // 追加したソースコード
+    DrawCircle(x, 0, 150, kColorBlue);
 }
 
